011fa0cc1922f72b40a963b20a956cbe049cd506
hamzahashmi3 <technocannons3@gmail.com>
2022-09-15 18:20:22 +0500

    undefined feat(ToDo List): multiple input onChange Handle

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,45 @@
-import React from 'react';
-import Header from "../components/home/Header";
-const Home = props => {
-    return (
-        <>
-            <Header />
-        </>
-    )
-}
+import React, { useState } from "react";
+import Header from "../components/Header";
+import CreateArea from "../components/CreateArea";
+import Note from "../components/Note";
+import Footer from "../components/Footer";
 
-export default Home
+const Home = (props) => {
+  const [notes, setNotes] = useState([]);
+
+  const addNotes = (newNote) => {
+    setNotes((prevNotes) => {
+      return [...prevNotes, newNote];
+    });
+    console.log(notes);
+  };
+
+  const onDelete = (id) => {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  };
+
+  return (
+    <>
+      <Header />
+      <CreateArea addNotes={addNotes} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={onDelete}
+          />
+        );
+      })}
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
